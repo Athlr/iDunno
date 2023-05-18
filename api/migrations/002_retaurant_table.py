b7d5d64b302be7e439a01bce39e4137a29d1a648
justinburgonio <burgoniojustin@yahoo.com>
2023-05-18 00:41:15 +0000

    Added tables for restaurants

---
+++ api/migrations/002_retaurant_table.py
@@ -0,0 +1,19 @@
+steps = [
+    [
+        ##"UP" SQL statement
+        """
+        CREATE TABLE restaurant(
+            restaurant_id SERIAL PRIMARY KEY NOT NULL,
+            name VARCHAR(1000) NOT NULL,
+            price VARCHAR(4) NOT NULL,
+            cuisine_id INT REFERENCES cuisine(cuisine_id) NOT NULL,
+            user_id INT REFERENCES user(user_id) NOT NULL,
+            suggested BOOLEAN DEFAULT FALSE,
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE restaurant;
+        """
+    ]
+]
