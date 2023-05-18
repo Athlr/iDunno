9db4c030550992c75556236bfb21be06d8d3156b
Aaron Tran <aatran2021@gmail.com>
2023-05-18 01:13:35 +0000

    Resolve "Make Friend Request and Friendships SQL Tables"

---
+++ api/migrations/006_friends.py
@@ -0,0 +1,31 @@
+steps = [
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE friend_request (
+            request_id SERIAL PRIMARY KEY NOT NULL,
+            status VARCHAR(100) DEFAULT 'pending' NOT NULL,
+            sender_id INT REFERENCES user(user_id) NOT NULL,
+            receiver_id INT REFERENCES user(user_id) NOT NULL
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE friend_request;
+        """
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE friendships (
+            friendship_id SERIAL PRIMARY KEY NOT NULL,
+            user1_id INT REFERENCES user(user_id) NOT NULL,
+            user2_id INT REFERENCES user(user_id) NOT NULL
+        );
+        """,
+        # "Down" SQL statemnet
+        """
+        DROP TABLE friendships;
+        """
+    ]
+]
