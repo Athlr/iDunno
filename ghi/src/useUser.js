89378f32139023fcb0776b0e2bb411c1553d365c
Matthew Huff <mhuff9112@gmail.com>
2023-05-23 22:58:13 -0400

    - Installed Dependency and jwtdown - Imported and added BrowserRouter and AuthProvider to index.js - created useUser Hook in src as useUser.js

---
+++ ghi/src/useUser.js
@@ -0,0 +1,28 @@
+
+
+
+
+const useUser = (token) => {
+    cosnt [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const getUser = async () => {
+            const response = await fetch(`${preocess.env.REACT_APP_API_HOST}/token`, {
+                headers:{
+                    Authorization: `bearer ${token}`,
+                }
+            });
+            const result = await response.json();
+            setUser(result.account)
+        }
+
+        if(token){
+            getUser();
+        }
+    }, [token]);
+
+    return user;
+
+};
+
+export default userUser;
