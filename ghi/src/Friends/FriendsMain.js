ce75466865316495a92ca9118af509137fa267ef
Aaron Tran <aatran2021@gmail.com>
2023-05-28 20:36:02 +0000

    [Feature#18 | Issue#58] Added input field for adding users | Dynamically renders msg based on result

--- ghi/src/Friends/FriendsMain.js
+++ ghi/src/Friends/FriendsMain.js
@@ -6,6 +6,8 @@ export default function FriendsMain() {
   const [friendsList, setFriendsList] = useState([]);
   const [friendRequests, setFriendRequests] = useState([]);
   const [isRequestOpen, setIsRequestOpen] = useState(false);
+  const [addUsername, setAddUsername] = useState("");
+  const [message, setMessage] = useState("");
 
   const openRequest = () => {
     setIsRequestOpen(true);
@@ -43,6 +45,32 @@ export default function FriendsMain() {
       const data = await response.json();
       console.log("Requests", data);
       setFriendRequests(data);
+    }
+  };
+
+  const addFriend = async () => {
+    console.log(message);
+    setMessage("");
+    const url = `${process.env.REACT_APP_API_HOST}/requests`;
+
+    const data = {
+      receiver_username: addUsername,
+    };
+
+    const response = await fetch(url, {
+      credentials: "include",
+      method: "post",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      setMessage(data);
+      setAddUsername("");
     }
   };
 
@@ -129,6 +157,50 @@ export default function FriendsMain() {
                     >
                       X
                     </button>
+                  </div>
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Add a Friend
+                  </Dialog.Title>
+                  <hr></hr>
+                  <div className="relative my-2">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3">
+                      <svg
+                        aria-hidden="true"
+                        className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                        />
+                      </svg>
+                    </div>
+                    <input
+                      type="text"
+                      className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 focus:outline-0"
+                      placeholder="Add friend by Username..."
+                      value={addUsername}
+                      onChange={(e) => setAddUsername(e.target.value)}
+                      required
+                    />
+                    <button
+                      type="submit"
+                      className="text-white absolute top-0 right-0 h-full bg-blue-700 hover:bg-blue-800 font-medium rounded-r-lg text-sm px-4 py-2"
+                      onClick={addFriend}
+                    >
+                      Add Friend
+                    </button>
+                  </div>
+                  <div className="my-2 text-sm mb-2 text-rose-600">
+                    {message.message ? message.message : ""}
                   </div>
                   <Dialog.Title
                     as="h3"
