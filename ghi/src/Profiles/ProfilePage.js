7f43fd0abee33b8ca73df176ed0269b9040c26ce
Aaron Tran <aatran2021@gmail.com>
2023-06-03 02:14:36 +0000

    [Feature#15 | Issue#74] Edit page pre-loads existing data

--- ghi/src/Profiles/ProfilePage.js
+++ ghi/src/Profiles/ProfilePage.js
@@ -126,7 +126,7 @@ export default function ProfilePage() {
               </div>
               <input
                 type="text"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-black focus:border-black block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search..."
                 onChange={(e) => setSearch(e.target.value)}
               />
@@ -145,6 +145,7 @@ export default function ProfilePage() {
                     picture={list.list_picture}
                     name={list.name}
                     description={list.description}
+                    fetchRestaurantListData={() => fetchRestaurantListData()}
                   />
                 );
               })}
