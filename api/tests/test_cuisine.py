38144ae7f2f862f31f5abb4ed58837176232871f
Matthew Huff <mhuff9112@gmail.com>
2023-06-07 13:19:43 -0400

    Handled eslint errors part 2

--- api/tests/test_cuisine.py
+++ api/tests/test_cuisine.py
@@ -1,13 +1,13 @@
-from fastapi.testclient import TestClient
-from pydantic import BaseModel
-from authenticator import authenticator
-from typing import Optional
-from main import app
-from queries import cuisine_list
+# from fastapi.testclient import TestClient
+# from pydantic import BaseModel
+# from authenticator import authenticator
+# from typing import Optional
+# from main import app
+# from queries import cuisine_list
 
-client = TestClient(app)
+# client = TestClient(app)
 
-class CuisineOut(BaseModel):
-    cuisine_id = int
-    name: str
+# class CuisineOut(BaseModel):
+#     cuisine_id = int
+#     name: str
     
