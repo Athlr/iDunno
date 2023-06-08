dbaccc21ff15aa1b4c8b6d2f92d0285a3c9c5950
Matthew Huff <mhuff9112@gmail.com>
2023-06-08 00:23:38 +0000

    Cuisine branch

---
+++ api/tests/test_cuisine_list.py
@@ -0,0 +1,31 @@
+from fastapi.testclient import TestClient
+from pydantic import BaseModel
+from typing import Union, List
+from main import app
+from queries.cuisine_list import CuisineListRepository, Error
+
+client = TestClient(app)
+
+
+class CuisineOut(BaseModel):
+    cuisine_id: int
+    name: str
+
+
+class EmptyCuisineRepository:
+    def get(self) -> Union[Error, List[CuisineOut]]:
+        return [CuisineOut(cuisine_id=1, name="name")]
+
+
+def test_get_all_cuisines():
+    app.dependency_overrides[CuisineListRepository] = EmptyCuisineRepository
+    expected = [{
+        "cuisine_id": 1,
+        "name": "name",
+    }]
+
+    response = client.get("/cuisine-list")
+    app.dependency_overrides = {}
+
+    assert response.status_code == 200
+    assert response.json() == expected
